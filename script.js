525eb0a7cb258c5a579f47c839bbd2a738767bad
laura930 <174218186+laura930@users.noreply.github.com>
2024-09-02 23:56:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+// Array extraño
+const strangeArray = [
+  "Zero",
+  function () {
+    alert("Hola soy una función en un array");
+  },
+  22,
+  null,
+  "Go lang",
+  undefined,
+  "Cobol",
+  "I'm programmer",
+  -2000,
+  "Hello world",
+  `One is ${1}`,
+  { name: "Info", lastname: "last info" },
+  () => true,
+  function showNumbers() {
+    return "1, 2, 3, 4";
+  },
+  "Another String",
+  ["Hola mundo!"],
+  "b is a letter",
+  "JavaScript",
+];
+
+// Función que recibe por parámetro un array y muestra sus elementos en pantalla
+function showList(array) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+  // Más info de forEach => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+  array.forEach((element) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(element));
+    container.appendChild(li);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  // Escribe tu solución aquí
+  // Sugerencia de cómo mostrar el array => showList(strangeArray);
+  let filtrar = strangeArray.filter((element) => typeof(element)=== "string") //filtra solo los textos
+
+  console.log(filtrar)
+  let alfa = filtrar.sort(function(a, b){  //los ordena alfabeticamente
+    if(a.toLowerCase() < b.toLowerCase()) { return -1; }
+    if(a.toLowerCase() > b.toLowerCase()) { return 1; }
+    return 0;
+})
+console.log(alfa);
+
+ showList(alfa) //usa la función dada anteriormente con el array alfa como parámetro
+
+});
